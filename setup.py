38838a9b05cbc48de563e5511f4275ad03539f85
jmg <jmg@nve.no>
2019-08-05 17:54:25 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='waterflows',
+      version='0.1',
+      description='Hydrological modelling in Python',
+      url='https://github.com/jannefiluren/waterflows',
+      author='Jan Magnusson',
+      author_email='jan.magnusson@gmail.com',
+      license='MIT',
+      packages=['waterflows'],
+      zip_safe=False)
