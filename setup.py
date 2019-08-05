6c16c39e3f4fcb7768f015f34acf67682b20886f
jmg <jmg@nve.no>
2019-08-05 20:08:46 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import codecs
+import os
 from setuptools import setup
+
+
+def read_file(name):
+    with codecs.open(
+            os.path.join(os.path.dirname(__file__), name), 'r', 'utf-8') as f:
+        return f.read().strip()
+
 
 setup(name='waterflows',
       version='0.1',
@@ -8,4 +17,5 @@ setup(name='waterflows',
       author_email='jan.magnusson@gmail.com',
       license='MIT',
       packages=['waterflows'],
+      install_requires=read_file('requirements.txt').splitlines(),
       zip_safe=False)
