ecfb0d46425477bfa77865771531e3dc874edb4c
jannefiluren <jan.magnusson@gmail.com>
2019-08-06 15:08:47 +0200

    refactored code and added step function

--- tests/test_snowmodel.py
+++ tests/test_snowmodel.py
@@ -1,42 +1,51 @@
 from unittest import TestCase
 
-from waterflows.snow import SnowModel
+from waterflows.snow import States, Inputs, Params, SnowModel
 
 class TestSnowModel(TestCase):
 
     def test_split_precipitation(self):
 
-        model = SnowModel()
+        model = SnowModel(States(), Params(), Inputs(-5, 10))
 
-        res = model.split_precipitation(10.0, -5.0, 0.0)
+        res = model.split_precipitation()
         self.assertTupleEqual(res, (10, 0))
 
-        res = model.split_precipitation(10.0, 5.0, 0.0)
+        model = SnowModel(States(), Params(), Inputs(5 ,10))
+
+        res = model.split_precipitation()
         self.assertTupleEqual(res, (0, 10))
+
 
     def test_compute_potmelt(self):
 
-        model = SnowModel()
+        model = SnowModel(States(), Params(), Inputs(10, 5))
 
-        pot_melt = model.compute_potmelt(10.0, 5.0, 0.0, 1.0)
+        pot_melt = model.compute_potmelt()
         self.assertEqual(pot_melt, 50.0)
 
-        pot_melt = model.compute_potmelt(-10.0, 5.0, 0.0, 1.0)
+        model = SnowModel(States(), Params(), Inputs(-10, 5))
+
+        pot_melt = model.compute_potmelt()
         self.assertEqual(pot_melt, 0.0)
+
 
     def test_compute_compute_potrefreeze(self):
 
-        model = SnowModel()
+        model = SnowModel(States(), Params(), Inputs(10, 5))
 
-        pot_refreeze = model.compute_potrefreeze(10.0, 5.0, 0.1, 0.0, 1.0)
+        pot_refreeze = model.compute_potrefreeze()
         self.assertAlmostEqual(pot_refreeze, 0.0, 5)
 
-        pot_refreeze = model.compute_potrefreeze(-10.0, 5.0, 0.1, 0.0, 1.0)
+        model = SnowModel(States(), Params(), Inputs(-10, 5))
+
+        pot_refreeze = model.compute_potrefreeze()
         self.assertAlmostEqual(pot_refreeze, 5.0, 5)
+
 
     def test_limit_fluxes(self):
 
-        model = SnowModel()
+        model = SnowModel(States(), Params(), Inputs(10, 5))
 
         act_flux = model.limit_fluxes(50, 100)
         self.assertAlmostEqual(act_flux, 50, 5)
@@ -49,29 +58,54 @@ class TestSnowModel(TestCase):
 
         # Outflow = 0
 
-        model = SnowModel()
-
-        model.fw, model.lw = 90.0, 5.0
+        states = States()
+        params = Params()
+        inputs = Inputs(10, 5)
+       
+        states.fw, states.lw = 90.0, 5.0
         psolid, pliquid, act_melt, act_refreeze = 10.0, 5.0, 5.0, 5.0
-        lwmax = 0.1
 
-        outflow = model.update_states(psolid, pliquid, act_melt, act_refreeze, lwmax)
+        model = SnowModel(states, params, inputs)
 
-        self.assertAlmostEqual(model.fw, 100.0, 8)
-        self.assertAlmostEqual(model.lw, 10.0, 8)
+        outflow = model.update_states(psolid, pliquid, act_melt, act_refreeze)
+
+        self.assertAlmostEqual(model.states.fw, 100.0, 8)
+        self.assertAlmostEqual(model.states.lw, 10.0, 8)
         self.assertAlmostEqual(outflow, 0.0, 8)
 
         # Outflow = 10
 
-        model.fw, model.lw = 90.0, 15.0
+        states.fw, states.lw = 90.0, 15.0
         psolid, pliquid, act_melt, act_refreeze = 10.0, 5.0, 5.0, 5.0
-        lwmax = 0.1
 
-        outflow = model.update_states(psolid, pliquid, act_melt, act_refreeze, lwmax)
+        model = SnowModel(states, params, inputs)
 
-        self.assertAlmostEqual(model.fw, 100.0, 8)
-        self.assertAlmostEqual(model.lw, 10.0, 8)
+        outflow = model.update_states(psolid, pliquid, act_melt, act_refreeze)
+
+        self.assertAlmostEqual(model.states.fw, 100.0, 8)
+        self.assertAlmostEqual(model.states.lw, 10.0, 8)
         self.assertAlmostEqual(outflow, 10.0, 8)
+
+
+
+    def test_step(self):
+
+        params = Params()
+
+        prec, temp, fw, lw = 10, 10, 0, 0
+
+        states = States(fw, lw)
+        inputs = Inputs(temp, prec)
+
+        model = SnowModel(states, params, inputs)
+
+        total_water_before = prec + model.states.total_water()
+
+        outflow = model.step()
+
+        total_water_after = outflow + model.states.total_water()
+
+        self.assertAlmostEqual(total_water_before, total_water_after, 8)
 
 
 
--- waterflows/snow.py
+++ waterflows/snow.py
@@ -1,31 +1,58 @@
-class SnowModel:
+class States:
+
+    def __init__(self, fw = 0, lw = 0):
+        self.fw = fw
+        self.lw = lw
+
+    def total_water(self):
+        return self.fw + self.lw
+
+class Params:
 
     def __init__(self):
-        self.fw = 0
-        self.lw = 0
+        self.tx = 0            # Precipitation phase threshold temperature
+        self.cx = 5            # Degreeday factor
+        self.ts = 0            # Melt onset threshold temperature
+        self.cfr = 0.1         # Refreeze factor
+        self.lwmax = 0.1       # Maximum waterholding capacity
+        self.step_in_days = 1  # Time step length in days
+        
+class Inputs:
+
+    def __init__(self, temp, prec):
+        self.temp = temp
+        self.prec = prec
 
 
-    def split_precipitation(self, prec, temp, tx):
-        if temp < tx:
-            psolid, pliquid = prec, 0
+class SnowModel:
+
+    def __init__(self, states, params, inputs):
+        self.states = states
+        self.params = params
+        self.inputs = inputs
+
+
+    def split_precipitation(self):
+        if self.inputs.temp < self.params.tx:
+            psolid, pliquid = self.inputs.prec, 0
         else:
-            psolid, pliquid = 0, prec
+            psolid, pliquid = 0, self.inputs.prec
         return psolid, pliquid
 
 
-    def compute_potmelt(self, temp, cx, ts, step_in_days):
-        if (temp - ts > 0):
-            pot_melt = cx * step_in_days * (temp - ts)
+    def compute_potmelt(self):
+        if self.inputs.temp - self.params.ts > 0:
+            pot_melt = self.params.cx * self.params.step_in_days * (self.inputs.temp - self.params.ts)
         else:
             pot_melt = 0
         return pot_melt
 
 
-    def compute_potrefreeze(self, temp, cx, cfr, ts, step_in_days):
-        if temp - ts > 0:
+    def compute_potrefreeze(self):
+        if self.inputs.temp - self.params.ts > 0:
             refreeze = 0
         else:
-            refreeze = -cfr * cx * step_in_days * (temp - ts)
+            refreeze = -self.params.cfr * self.params.cx * self.params.step_in_days * (self.inputs.temp - self.params.ts)
         return refreeze
 
 
@@ -33,21 +60,35 @@ class SnowModel:
         return min(available_water, flux_value)
         
     
-    def update_states(self, psolid, pliquid, act_melt, act_refreeze, lwmax):
-        self.fw += psolid + act_refreeze - act_melt
-        self.lw += pliquid + act_melt - act_refreeze
+    def update_states(self, psolid, pliquid, act_melt, act_refreeze):
+        self.states.fw += psolid + act_refreeze - act_melt
+        self.states.lw += pliquid + act_melt - act_refreeze
 
-        if (self.lw > self.fw * lwmax):
-            outflow = self.lw - self.fw * lwmax
-            self.lw = self.fw * lwmax
+        if (self.states.lw > self.states.fw * self.params.lwmax):
+            outflow = self.states.lw - self.states.fw * self.params.lwmax
+            self.states.lw = self.states.fw * self.params.lwmax
         else:
             outflow = 0.0
 
         return outflow
 
 
-    def step(self, prec_mm_h, temp):
-        pass
+    def step(self):
+        
+        pliquid, psolid = self.split_precipitation()
+
+        melt = self.compute_potmelt()
+
+        refreeze = self.compute_potrefreeze()
+
+        melt = self.limit_fluxes(melt, self.states.fw)
+
+        refreeze = self.limit_fluxes(refreeze, self.states.lw)
+        
+        outflow = self.update_states(psolid, pliquid, melt, refreeze)
+
+        return outflow
+
 
 
 
